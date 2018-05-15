74bc61a581065bae7ff375dd1c0b5059a34f1d3e
Ravi Mahankali <ravikm@ebi.ac.uk>
2018-05-16 00:56:28 +0100

    final commit

--- subnetter.sh
+++ subnetter.sh
@@ -12,26 +12,36 @@ d=$2
 sub=`echo $1 | cut -d / -f2`
 
 b=0
-m=$((32-sub))
-c=$((2**$m))
 
 if [[ "$sub" -ge "24" ]]
 then
-    li=$((ip4+c-1))
+    x=4
+    m=$((32-sub))
+    c=$((2**$m))
+    li=$((ip$x+c-1))
 elif [[ "$sub" -ge "16" && "$sub" -le "23" ]]
 then
-    li=$((ip3+c-1))
+    x=3
+    m=$((24-sub))
+    c=$((2**$m))
+    li=$((ip$x+c-1))
 elif [[ "$sub" -ge "8" && "$sub" -le "15" ]]
 then
-    li=$((ip2+c-1))
+    x=2
+    m=$((16-sub))
+    c=$((2**$m))
+    li=$((ipx+c-1))
 elif [[ "$sub" -ge "0" && "$sub" -le "7" ]]
 then
-    li=$((ip1+c-1))
+    x=1
+    m=$((8-sub))
+    c=$((2**$m))
+    li=$((ipi$x+c-1))
 fi
 
 for r in $(eval echo {$r..1} )
 do
-n=$((32-sub))
+n=$m
 q=$(($c/$r))
   while [[ "$q" -le "$((2**$n))" && "$n" -gt "0" ]]
   do
@@ -40,7 +50,7 @@ q=$(($c/$r))
         p=$((2**$n))
         c=$((c-p))
         sn=$((li-c+1))
-        sb=$((32-n))
+        sb=$((8*$x-n))
 for j in $(eval echo {$n..1} )
 do
 s=$((2**($j-1)))
@@ -57,14 +67,14 @@ then
 elif [[ "$sub" -ge "16" && "$sub" -le "23" ]]
 then
     v=$ip3
-echo "subnet=$ip1.$ip2.$((sn-p)).$ip4/sb" "network=$ip1.$ip2.$((sn-p)).$ip4" "broadcast=$ip1.$ip2.$((sn-1)).$ip4" "gateway=$ip1.$ip2.$((sn-p+1)).$ip4" "hosts=$(($p-3))"
+echo "subnet=$ip1.$ip2.$((sn-p)).$ip4/$sb" "network=$ip1.$ip2.$((sn-p)).$ip4" "broadcast=$ip1.$ip2.$((sn-1)).255" "gateway=$ip1.$ip2.$((sn-p+1)).$ip4" "hosts=$(($p-3))"
 elif [[ "$sub" -ge "8" && "$sub" -le "15" ]]
 then
     v=$ip2
-echo "subnet=$ip1.$((sn-p)).$ip3.$ip4/sb" "network=$ip1.$((sn-p)).$ip3.$ip4" "broadcast=$ip1.$((sn-1)).$ip3.$ip4" "gateway=$ip1.$((sn-p+1)).$ip3.$ip4" "hosts=$(($p-3))"
+echo "subnet=$ip1.$((sn-p)).$ip3.$ip4/$sb" "network=$ip1.$((sn-p)).$ip3.$ip4" "broadcast=$ip1.$((sn-1)).255.255" "gateway=$ip1.$((sn-p+1)).$ip3.$ip4" "hosts=$(($p-3))"
 elif [[ "$sub" -ge "0" && "$sub" -le "7" ]]
 then
     v=$ip1
-echo "subnet=$((sn-p)).$ip2.$ip3.$ip4/sb" "network=$((sn-p)).$ip2.$ip3.$ip4" "broadcast=$((sn-1)).$ip2.$ip3.$ip4" "gateway=$((sn-p+1)).$ip2.$ip3.$ip4" "hosts=$(($p-3))"
+echo "subnet=$((sn-p)).$ip2.$ip3.$ip4/$sb" "network=$((sn-p)).$ip2.$ip3.$ip4" "broadcast=$((sn-1)).255.255.255" "gateway=$((sn-p+1)).$ip2.$ip3.$ip4" "hosts=$(($p-3))"
 fi
 done
